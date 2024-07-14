e9380f076e91216ff15a25bb1ba9e8d2c2a3bcd5
Andrea Bazerla <andrea.bazerla@gmail.com>
2024-07-14 09:37:16 +0200

    Centered the header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,8 +3,8 @@ import { Col, Row } from "react-bootstrap";
 const Header = () => {
   return (
     <header style={{height:'15vh'}}>
-      <Row>
-        <Col xs={12}>
+      <Row className='justify-content-center'>
+        <Col sm={8} md={8} lg={6} xl={4} xxl={2}>
           <h1>Quit2Invest</h1>
           <h5>Smetti di fumare ed inizia a investire!</h5>
         </Col>
